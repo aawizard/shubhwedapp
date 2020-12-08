0c27f83b27d363f135ce18063ce0ae84d2a07d42
aawzard <iec2019097@iiita.ac.in>
2020-12-08 15:35:50 +0530

    added some files and constants

---
+++ lib/util/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color kPink = Color(0xffFF5861);
+Color kGrey = Color(0xffE5E5E5);
+Color kDarkPink =Color(0xffee3e83);
+Color kSkin = Color(0xffffddcc);
+
+List<Color> kBgGradient = [
+  kDarkPink,
+  kSkin,
+];
